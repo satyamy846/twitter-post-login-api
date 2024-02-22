1f68a7b5a9cce0ce3b539dc9dda6e7eb409286ef
code-commit-demo <satyadav941@gmail.com>
2024-02-22 21:47:10 +0530

    some changes

--- index.js
+++ index.js
@@ -10,62 +10,77 @@ const app = express();
 
 // Initialize auth client first
 const authClient = new auth.OAuth2User({
-  client_id: process.env.TWITTER_CLIENT_ID,
-  callback: "http://localhost:3000/callback",
-  scopes: ["tweet.read", "users.read", "tweet.write"],
+    client_id: process.env.TWITTER_CLIENT_ID,
+    callback: "http://localhost:3000/callback", // your callback url should be added to your twitter developer settings
+    scopes: ["tweet.read", "users.read", "tweet.write"], // scopes can be
 });
 
- // Pass auth credentials to the library client 
- const client = new Client(authClient);
+// Pass auth credentials to the library client 
+const client = new Client(authClient);
 
 
- const STATE = "my-state";
+const STATE = "my-state";
 
- app.get("/", (req, res) =>{
-  res.send(`<a href="/login" >Login to twitter</a>`)
- })
-
- app.get("/success", (req, res)=>{
-  res.send(`Login successfull <br/> <a href="/upload-media" >Upload your media</a>`);
- })
-
-
-let access_token = "";
- app.get("/upload-media", async (req, res) =>{
-  const response = await client.tweets.createTweet({
-    text: "Hi this is my first tweet",
+/**
+ * Home route
+ */
+app.get("/", (req, res) => {
+    res.send(`<a href="/login" >Login to twitter</a>`)
 })
-  console.log("tweet posted");
-  res.send(response);
- })
-
- app.get("/callback", async function (req, res) {
-   try {
-     const { code, state } = req.query;
-
-     if (state !== STATE) return res.status(500).send("State isn't matching");
-     const response = await authClient.requestAccessToken(code.toString());
-     console.log(response);
-     res.redirect("/success");
-   } catch (error) {
-     console.log(error);
-   }
- });
- 
 
 
- app.get("/login", async function (req, res) {
-   const authUrl = authClient.generateAuthURL({
-     state: STATE,
-     code_challenge_method: "s256",
-   });
-   res.redirect(authUrl);
- });
- 
-
-
-app.listen(3000, () =>{
-    console.log(`Server is running on port ${3000}`);
+/**
+ * Login with Twitter route
+ */
+app.get("/login", async function (req, res) {
+    const authUrl = authClient.generateAuthURL({
+        state: STATE,
+        code_challenge_method: "s256",
+    });
+    res.redirect(authUrl);
 });
 
- 
+/**
+ * Redirect to callback url route
+ */
+
+app.get("/callback", async function (req, res) {
+    try {
+        const { code, state } = req.query;
+
+        if (state !== STATE) return res.status(500).send("State isn't matching");
+        // request access token from twitter api for authenticating user
+        const response = await authClient.requestAccessToken(code.toString());
+        console.log(response);
+        res.redirect("/success");
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+/**
+ * Login success route
+ */
+app.get("/success", (req, res) => {
+    res.send(`Login successfull <br/> <a href="/upload-tweet" >Upload your media</a>`);
+})
+
+/**
+ * upload tweet route 
+ */
+app.get("/upload-tweet", async (req, res) => {
+    // createTweet method used to make a tweet 
+    // media _ids is an array of media ids which you want to add in the tweet afer uploading media at the twitter server
+    const response = await client.tweets.createTweet({
+        text: "Hi this is my first tweet",
+    })
+    console.log("tweet posted");
+    res.send(response);
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port htttp://localhost:${process.env.PORT}`);
+});
+
